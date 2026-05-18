52d48d08e43b614eb107b281f8535f26d1ada8a0
Louidporto <135027792+Louidporto@users.noreply.github.com>
2026-05-18 17:46:12 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCRIoKEfFU5LqBkszVgeZDfC2eiJsTtvkI",
   authDomain: "fruto-proibido-73c54.firebaseapp.com",
+  databaseURL: "https://fruto-proibido-73c54-default-rtdb.firebaseio.com",
   projectId: "fruto-proibido-73c54",
   storageBucket: "fruto-proibido-73c54.firebasestorage.app",
   messagingSenderId: "1067844602315",
   appId: "1:1067844602315:web:b6f50d81fae62e15ee4e4e"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Atalho para o Banco de Dados
+const database = firebase.database();
+
